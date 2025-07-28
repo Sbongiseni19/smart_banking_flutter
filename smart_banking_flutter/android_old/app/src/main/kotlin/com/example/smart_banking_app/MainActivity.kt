80e631b13bdb8ca3d4ad82ea7fd27b07f2a669be
Sbongiseni Ngema <ngemasbongiseni00@gmail.com>
2025-07-28 19:14:30 +0200

    Validating registration form with passport field

--- smart_banking_flutter/android_old/app/src/main/kotlin/com/example/smart_banking_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.smart_banking_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- smart_banking_flutter/lib/screens/booking_screen.dart
+++
@@ -1,9 +0,0 @@
-// lib/services/booking_data.dart
-
-// Global list of bookings (in-memory)
-List<Map<String, String>> bookings = [];
-
-// Function to add a booking to the list
-void addBooking(Map<String, String> booking) {
-  bookings.add(booking);
-}
